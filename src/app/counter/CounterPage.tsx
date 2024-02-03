36e29451dc319c8c6d0258ac820e845b7a200750
wiroon.sri@C-709 <wiroon.sri@innosoft.kmutt.ac.th>
2024-02-03 15:22:24 +0000

    change url

--- src/app/counter/CounterPage.tsx
+++ src/app/counter/CounterPage.tsx
@@ -6,18 +6,30 @@ import CounterContainer from "@/components/CounterContainer"
 import CounterLabel from "@/components/CounterLabel"
 import Link from "next/link"
 import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 
 function CounterPage() {
     const router = useRouter()
+    const searchParams = useSearchParams()
 
-    return (<>
-        <CounterContainer></CounterContainer>
-        <div><Link href="counter2" replace={true}>Goto Count 2</Link></div>
-        <div><button onClick={() => router.push('/counter2', { shallow: true })}>
-            goto counter 2
-        </button></div>
+    const changeLink = () => {
+        console.log('changeLink')
+        router.push('counter2', { scroll: true })
+    }
 
-    </>)
+    return (
+        <>
+            <CounterContainer></CounterContainer>
+            <div>
+                <Link href="counter2" replace={true}>Goto Count 2</Link>
+            </div>
+            <div>
+                <button onClick={changeLink}>
+                    goto counter 2
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default CounterPage
--- src/app/counter2/Counter2Page.tsx
+++ src/app/counter2/Counter2Page.tsx
@@ -5,13 +5,26 @@ import Counter1 from "@/components/Counter1"
 import CounterContainer from "@/components/CounterContainer"
 import CounterLabel from "@/components/CounterLabel"
 import Link from "next/link"
+import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 
 function Counter2Page() {
+    const router = useRouter()
+
+    const changeLink = () => {
+        console.log('changeLink')
+        router.push('counter', { scroll: true })
+    }
 
     return (<>
         <div>Counter 2 Page</div>
         <CounterContainer></CounterContainer>
         <Link href="counter" replace={true}>Goto Count</Link>
+        <div>
+            <button onClick={changeLink}>
+                goto counter
+            </button>
+        </div>
     </>)
 }
 
